2756829e281461f2bd55302f4d43473874ff2dbe
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2026-01-22 19:28:37 +0100

    first component change in React

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,33 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to the App</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
